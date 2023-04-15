d0b9af250d8b8f5de4f32c87631a144113bbac2f
Abner Kaizer <abnerkaizer@protonmail.com>
2023-04-15 10:20:25 -0300

    api call

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,23 @@
+import { useState, useEffect } from "react";
+
+const movieURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Home = () => {
+    const [topMovies, setTopMovies] = useState([]);
+    const getTopRatedMovies = async(url) => {
+        const resp = await fetch(url);
+        const data = await resp.json();
+        setTopMovies(data.results);
+    };
+    useEffect(()=>{
+        const topRatedUrl = `${movieURL}top_rated?${apiKey}`;
+        getTopRatedMovies(topRatedUrl);
+    },[]);
     return(
-        <>
-            <p>
-                Home
-            </p>
-        </>
+        <div>
+            {topMovies && topMovies.map((movie)=><p>{movie.title}</p>)}
+        </div>
     )
 }
 
