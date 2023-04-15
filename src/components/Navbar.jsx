158b4f3108c6b3345c5e3dab32b042fd0f576b52
Abner Kaizer <abnerkaizer@protonmail.com>
2023-04-15 17:39:02 -0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import {BiCameraMovie, BiSearchAlt2} from 'react-icons/bi'
+import './Navbar.css'
+
 
 const Navbar = () => {
   return (
@@ -8,8 +10,6 @@ const Navbar = () => {
         <h2>
         <Link to="/"><BiCameraMovie/>IMDB clone</Link>
         </h2>
-        <Link to="/movie/1">Movie</Link>
-        <Link to="/search">Search</Link>
         <form action="">
             <input type="text" placeholder='Busque um filme'/>
             <button type='submit'><BiSearchAlt2/></button>
